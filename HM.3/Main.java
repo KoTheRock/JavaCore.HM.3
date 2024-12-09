c1b5ade0afe96b17870d9e5740d8a579843ac811
KoTheRock <149530900+KoTheRock@users.noreply.github.com>
2024-12-09 17:23:03 +0300

    Delete HM.3 directory

--- HM.3/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        int result = Worker.compareDates(2024, 12, 9, 2017, 7, 3);
-        System.out.println(result > 0 ? "Первая дата позже" : result < 0 ? "Первая дата раньше" : "Даты равны");
-    }
-}
